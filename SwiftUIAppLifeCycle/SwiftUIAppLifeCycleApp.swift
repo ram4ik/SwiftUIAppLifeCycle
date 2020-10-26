35ec4f6d5a60eba4f168e211c7a14e9bb9cd8fa9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-26 13:22:22 +0200

    init

--- SwiftUIAppLifeCycle/SwiftUIAppLifeCycleApp.swift
+++ SwiftUIAppLifeCycle/SwiftUIAppLifeCycleApp.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 @main
 struct SwiftUIAppLifeCycleApp: App {
+    
+    @Environment(\.scenePhase) var scenePhase
+    
+    init() {
+        // do init part
+        doFirebaseInit()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.onChange(of: scenePhase) { (phase) in
+            switch phase {
+            
+            case .background:
+                print("background phase")
+            case .inactive:
+                print("inactive phase")
+            case .active:
+                print("active phase")
+            @unknown default:
+                print("default phase")
+            }
         }
+    }
+    
+    func doFirebaseInit() {
+        // ...
     }
 }
